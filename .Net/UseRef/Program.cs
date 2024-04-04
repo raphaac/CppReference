ba28889eab4da972903773c8173eac659f809a52
Raphael Correa <raphael.abrantes@trieengineering.com>
2024-04-04 11:13:16 -0300

    test: first clean and basci version

--- .Net/UseRef/Program.cs
+++ .Net/UseRef/Program.cs
@@ -4,15 +4,26 @@ namespace UseRef
 {
     internal class Program
     {
-        [DllImport("Sum.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int soma(int a, int b);
+        private static class RaphaelMethods
+        {
+            private const string DllName = "Sum.dll";
+
+            [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+            public static extern int soma(int a, int b);
+
+            [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+            public static extern int multiplicacao(int a, int b);
+        }
 
         static void Main(string[] args)
         {
-            int resultado = soma(5, 3); 
-            Console.WriteLine($"O resultado é: {resultado}");
+            int resultado = RaphaelMethods.soma(5, 3); 
+            Console.WriteLine($"O resultado da soma é: {resultado}");
+
+            resultado = RaphaelMethods.multiplicacao(5, 3);
+            Console.WriteLine($"O resultado da multiplicação é: {resultado}");
+
             Console.ReadLine();
         }
-
     }        
 }
--- Sum/Main.cpp
+++ Sum/Main.cpp
@@ -1,17 +1,16 @@
 #include "pch.h" // Use esta linha se o PCH estiver habilitado. Caso contrário, pode omitir.
 
 // Definindo a macro de exportação
-//#ifdef MINHASOMADLL_EXPORTS
-//#define MINHASOMADLL_API __declspec(dllexport)
-//#else
-//#define MINHASOMADLL_API __declspec(dllimport)
-//#endif
-
-#define MINHASOMADLL_API __declspec(dllimport)
+#define DLL_EXPORT __declspec(dllexport)
 
 extern "C" {
     // Função que será exportada
-    MINHASOMADLL_API int soma(int a, int b) {
+    DLL_EXPORT int soma(int a, int b) {
         return a + b;
+    }
+
+    // Função que será exportada
+    DLL_EXPORT int multiplicacao (int a, int b) {
+        return a * b;
     }
 }
