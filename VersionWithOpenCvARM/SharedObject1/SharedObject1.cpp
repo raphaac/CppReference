84943ccc98779ac4cda98a96da47fbf4b8c23522
Raphael Correa <raphael.abrantes@trieengineering.com>
2024-04-10 09:09:33 -0300

    test. With ARM eabi

---
+++ VersionWithOpenCvARM/SharedObject1/SharedObject1.cpp
@@ -0,0 +1,52 @@
+#include "SharedObject1.h"
+#include "opencv2/core.hpp"
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "SharedObject1", __VA_ARGS__))
+#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "SharedObject1", __VA_ARGS__))
+
+extern "C" {
+	/* This trivial function returns the platform ABI for which this dynamic native library is compiled.*/
+	const char * SharedObject1::getPlatformABI()
+	{
+	#if defined(__arm__)
+	#if defined(__ARM_ARCH_7A__)
+	#if defined(__ARM_NEON__)
+		#define ABI "armeabi-v7a/NEON"
+	#else
+		#define ABI "armeabi-v7a"
+	#endif
+	#else
+		#define ABI "armeabi"
+	#endif
+	#elif defined(__i386__)
+		#define ABI "x86"
+	#else
+		#define ABI "unknown"
+	#endif
+		LOGI("This dynamic shared library is compiled with ABI: %s", ABI);
+		return "This native library is compiled with ABI: %s" ABI ".";
+	}
+
+	void SharedObject1()
+	{
+	}
+
+	int MethodOpenCv()
+	{
+		cv::Mat img(10, 10, CV_8UC1);
+		return img.rows;
+	}
+
+	int Teste()
+	{
+		return 100;
+	}
+
+	SharedObject1::SharedObject1()
+	{
+	}
+
+	SharedObject1::~SharedObject1()
+	{
+	}
+}
---
+++ VersionWithOpenCvARMeabi/WithARMeabi/AndroidLib1/Class1.cs
@@ -0,0 +1,10 @@
+using System.Runtime.InteropServices;
+
+namespace AndroidLib1
+{
+    public static class Class1
+    {
+        [DllImport("libSharedObject1.so", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int MethodOpenCv();
+    }
+}
