390b5c5e7ac2b20bd39d7e1f8d635087b868e526
Raphael Correa <raphael.abrantes@trieengineering.com>
2024-04-16 11:15:25 -0300

    feat: test with lib aruco

--- PlayGround/VersionRaphael/CleanVersion/AndroidLib1/Class1.cs
+++ PlayGround/VersionRaphael/CleanVersion/AndroidLib1/Class1.cs
@@ -1,13 +1,45 @@
+using Android.Views;
 using System.Runtime.InteropServices;
+using UnityEngine;
 
 namespace AndroidLib1
 {
     public static class Class1
     {
-        [DllImport("libSharedObject1.so", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int NumberValidate();
+        private const string LibraryName = "libSharedObject1.so";
 
-        [DllImport("libSharedObject1.so", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int MethodOpenCv();        
+
+    }
+
+    public class CvManager : MonoBehaviour 
+    {
+        private const string LibraryName = "libSharedObject1.so";
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
         public static extern int MethodOpenCv();
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void InitOpenCvFrame(int width, int height);
+
+        [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void GetFeature(ref Color32[] rawData);
+
+        Texture2D _cameraTexture;
+        WebCamTexture _webCarmTexture;
+        public TextureView _cameraImage;
+
+
+        void Start()
+        {
+            _webCarmTexture = new WebCamTexture(640, 360, 30);
+            _webCarmTexture.Play();
+            _cameraTexture = new Texture2D(_webCarmTexture.width, _webCarmTexture.height);
+        }
+
+        void Update() 
+        {
+        }
     }
 }
--- PlayGround/VersionRaphael/CleanVersion/SharedObject1/SharedObject1.cpp
+++ PlayGround/VersionRaphael/CleanVersion/SharedObject1/SharedObject1.cpp
@@ -1,10 +1,42 @@
 #include "SharedObject1.h"
 #include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/aruco/aruco_calib.hpp"
 
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "SharedObject1", __VA_ARGS__))
 #define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN, "SharedObject1", __VA_ARGS__))
 
 extern "C" {
+
+	cv::Mat cameraFrame;
+
+	void InitOpenCvFrame(int width, int height)
+	{
+		cameraFrame = cv::Mat(height, width, CV_8UC4);
+	}
+
+	void GetFeature(unsigned char** rawImage)
+	{
+		cameraFrame.data = *rawImage;
+		cv::Mat grey;
+		cvtColor(cameraFrame, grey, 10);
+
+		std::vector<cv::Point2f> corners;
+
+		cv::goodFeaturesToTrack(grey, corners, 20, 0.01, 10, cv::Mat(), 3, false, 0.04);
+
+		for (size_t i = 0; i < corners.size(); i++)
+		{
+			cv::circle(cameraFrame, corners[i], 8, cv::Scalar(0, 255, 0, 0), 1);
+		}
+	}
+
+	void TestAruco()
+	{
+		int valor = cv::aruco::ARUCO_CCW_CENTER;
+	}
+
 	/* This trivial function returns the platform ABI for which this dynamic native library is compiled.*/
 	const char * SharedObject1::getPlatformABI()
 	{
@@ -25,11 +57,6 @@ extern "C" {
 	#endif
 		LOGI("This dynamic shared library is compiled with ABI: %s", ABI);
 		return "This native library is compiled with ABI: %s" ABI ".";
-	}
-
-	int NumberValidate() 
-	{
-		return 10;
 	}
 
 	int MethodOpenCv()
